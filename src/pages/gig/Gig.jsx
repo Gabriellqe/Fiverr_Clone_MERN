ef50059795d7a9b1cf80b48812608e09fa76e617
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-03-09 11:00:21 -0300

    Added gigs page

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Gig.scss";
+import GigCard from "../../components/gigCard/GigCard";
 const Gig = () => {
-  return <div className="gig">Add</div>;
+  return (
+    <div className="gig">
+      <GigCard />
+    </div>
+  );
 };
 export default Gig;
