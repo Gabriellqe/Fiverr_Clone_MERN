44482ef8f81497e2f84d5dd02a9fdffb1282c92a
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-04-11 15:30:22 -0400

    Added CRUD (users, auth)  routes and controllers

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,14 @@
+import userModel from "../models/user.model.js";
+import createError from "../utils/createError.js";
+
+export const fn = async (req, res) => {};
+
+export const deleteUser = async (req, res, next) => {
+  const user = await userModel.findById(req.params.id);
+  if (req.userId !== user._id.toString()) {
+    return next(createError(403, "You can delete only your account"));
+  }
+  await userModel.findByIdAndDelete(req.params.id);
+  res.status(200).send("deleted");
+};
+export const getUser = async (req, res) => {};
