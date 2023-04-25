f4374d615b348b611ba71c3d57d953332f962d95
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-04-25 15:26:49 -0400

    Added controllers to gig routes

--- api/routes/gig.routes.js
+++ api/routes/gig.routes.js
@@ -1,7 +1,16 @@
 import express from "express";
-import { fn } from "../controllers/user.controller.js";
+import {
+  createGig,
+  deleteGig,
+  getGig,
+  getGigs,
+} from "../controllers/gig.controller.js";
+import { verifyToken } from "../middlewares/jwt.js";
 const router = express.Router();
 
-router.route("/").get();
+router.post("/", verifyToken, createGig);
+router.delete("/:id", verifyToken, deleteGig);
+router.get("/single/:id", getGig);
+router.get("/", getGigs);
 
 export default router;
