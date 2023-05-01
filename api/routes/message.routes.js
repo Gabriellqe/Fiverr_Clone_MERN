e9b51bef73066b4a102ca3fdb913ca23a17cef77
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-04-30 23:02:17 -0400

    Added conversation and ordering functionalities

--- api/routes/message.routes.js
+++ api/routes/message.routes.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { fn } from "../controllers/user.controller.js";
+import {
+  createMessage,
+  getMessages,
+} from "../controllers/message.controller.js";
+import { verifyToken } from "../middlewares/jwt.js";
+
 const router = express.Router();
 
-router.route("/").get();
+router.post("/", verifyToken, createMessage);
+router.get("/:id", verifyToken, getMessages);
 
 export default router;
