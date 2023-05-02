cb7a840fb69ac1618109526e1812a66489d01bed
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-05-01 23:53:53 -0400

    Added payment controllers

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -2,25 +2,34 @@ import orderModel from "../models/order.model.js";
 import gigModel from "../models/gig.model.js";
 import createError from "../utils/createError.js";
 
-export const createOrder = async (req, res, next) => {
-  try {
-    const gig = await gigModel.findById(req.params.id);
-    console.log(gig);
-    const newOrder = new orderModel({
-      gigId: gig._id,
-      img: gig.cover,
-      title: gig.title,
-      buyerId: req.userId,
-      sellerId: gig.userId,
-      price: gig.price,
-      payment_intent: "TEMPORARY",
-    });
+export const intent = async (req, res, next) => {
+  const stripe = new Stripe(process.env.STRIPE);
 
-    await newOrder.save();
-    res.status(201).json("success");
-  } catch (error) {
-    next(error);
-  }
+  const gig = await gigModel.findById(req.params.id);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: gig.price * 100,
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  const newOrder = new orderModel({
+    gigId: gig._id,
+    img: gig.cover,
+    title: gig.title,
+    buyerId: req.userId,
+    sellerId: gig.userId,
+    price: gig.price,
+    payment_intent: paymentIntent.id,
+  });
+
+  await newOrder.save();
+
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret,
+  });
 };
 
 export const getOrders = async (req, res, next) => {
@@ -35,4 +44,21 @@ export const getOrders = async (req, res, next) => {
   }
 };
 
-export const confirm = async (req, res, next) => {};
+export const confirm = async (req, res, next) => {
+  try {
+    const orders = await orderModel.findOneAndUpdate(
+      {
+        payment_intent: req.body.payment_intent,
+      },
+      {
+        $set: {
+          isCompleted: true,
+        },
+      }
+    );
+
+    res.status(200).send("Order has been confirmed.");
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/order.routes.js
+++ api/routes/order.routes.js
@@ -1,13 +1,10 @@
 import express from "express";
-import {
-  getOrders,
-  createOrder,
-  confirm,
-} from "../controllers/order.controller.js";
+import { getOrders, intent, confirm } from "../controllers/order.controller.js";
 import { verifyToken } from "../middlewares/jwt.js";
 const router = express.Router();
 
-router.route("/").get(verifyToken, getOrders).put(verifyToken, confirm);
-router.route("/:id").post(verifyToken, createOrder);
+router.get("/", verifyToken, getOrders);
+router.post("/create-payment-intent/:id", verifyToken, intent);
+router.put("/", verifyToken, confirm);
 
 export default router;
